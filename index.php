1054e26a4083bfd7e628c5af5284552f1c2b54b0
BashlanovGI <99040636+BashlanovGI@users.noreply.github.com>
2022-08-30 20:06:39 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+echo "Enter Name: ";
+$name = fgets (stream: STDIN);
+
+echo "Enter Number: ";
+$number = fgets (stream: STDIN);
+
+$str = str_replace("\n", "", $name . " " . $number) . "\n***\n"; //Создаем строку и удаляем перенос строки оставшиеся с STDIN
+
+$file = "/home/camorra/Desktop/numBook.txt"; // Путь к файлу
+
+file_put_contents($file, $str, FILE_APPEND); //Записываем данные в файл
+
+$numBook = file_get_contents($file); //Получаем файл в переменную
+
+$numBook = explode("\n***\n", $numBook); //Создаем массив с помощью разделителя
+
+$numBook = array_diff ($numBook, array ('', '')); //Удаляем пустые элементы массива
+
+natcasesort($numBook); //Сортировка без учета регистра
+
+file_put_contents($file, ''); // Очищаем файл
+
+foreach($numBook as $num) {
+    file_put_contents($file, $num . "\n***\n", FILE_APPEND); // Записываем данные из массива в файл
+}
+
+?>
